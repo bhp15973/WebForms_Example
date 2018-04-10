0b12463b3719ddffb1ad06cffb90064ddc50d3f5
PGS-SOFT\bmaciejewski <bmaciejewski@pgs-soft.com>
2018-04-10 15:54:29 +0200

    add sln file

--- WebForms_Example/App_Code/PeopleDB.Designer.cs
+++ WebForms_Example/App_Code/PeopleDB.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\downloads\WebForms_Example\App_Code\PeopleDB.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\git\WebForms_Example\WebForms_Example\App_Code\PeopleDB.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
